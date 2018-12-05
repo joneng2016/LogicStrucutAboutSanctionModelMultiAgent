6dfe7bfa26f8edd39c71e1b8f7409ebe7d78b165
root <root@debian.jonathan>
2018-12-05 18:14:10 -0200

    inserindo os rgs

--- applicationphp/app/package/ProgramSetRelation.php
+++ applicationphp/app/package/ProgramSetRelation.php
@@ -42,6 +42,136 @@ class ProgramSetRelation extends AbstractProgramExecution
                     "relationcondutivimetrosoquete",
                 ],
                 "fileName" => "filerg2"
+            ],
+            [
+                "rg" => "rg3",
+                "goal" => "goal3",
+                "relation" => [
+                    "relationxcondutivimetro"
+                ],
+                "fileName" => "filerg3"
+            ],
+            [
+                "rg" => "rg4",
+                "goal" => "goal4",
+                "relation" => [
+                    "relationXbastaogarra",
+                    "relationcolarbastaogarra"
+                ],
+                "fileName" => "filerg4"
+            ],
+            [
+                "rg" => "rg5",
+                "goal" => "goal5",
+                "relation" => [
+                    "relationXbastaogarra",
+                    "relationXcorda",
+                    "relationcordabastaogarra",
+                    "relationbastaogarratorre",
+                    "relationbastaogarracondutor"
+                ],
+                "fileName" => "filerg5"
+            ],
+            [
+                "rg" => "rg6",
+                "goal" => "goal6",
+                "relation" => [
+                    "relationbastaogarrasela",
+                    "relationXbastaogarra",
+                    "relationXsela"
+                ],
+                "fileName" => "filerg6"
+            ],
+            [
+                "rg" => "rg7",
+                "goal" => "goal7",
+                "relation" => [
+                    "relationXsela",
+                    "relationXcolar",
+                    "relationtorresela",
+                ],
+                "fileName" => "filerg7"
+            ],
+            [
+                "rg" => "rg8",
+                "goal" => "goal8",
+                "relation" => [
+                    "relationbastaouniversalcorda",
+                    "relationXbastaouniversal"
+                ],
+                "fileName" => "filerg8"
+            ],
+            [
+                "rg" => "rg9",
+                "goal" => "goal9",
+                "relation" => [
+                    "relationXbataouniversal",
+                    "relationXcarretilha",
+                    "relationbastaouniversalcarretilha"
+                ],
+                "fileName" => "filerg9"
+            ],
+            [
+                "rg" => "rg10",
+                "goal" => "goal10",
+                "relation" => [
+                    "relationXcorda",
+                    "relationXbastaouniversal",
+                    "relationbastaouniversalcorda",
+                    "relationbastaouniversaltorre"
+                ],
+                "fileName" => "filerg10"
+            ],
+            [
+                "rg" => "rg11",
+                "goal" => "goal11",
+                "relation" => [
+                    "relationXcorda",
+                    "relationXbastaouniversal",
+                    "relationbastaouniversalcolar",
+                    "relationbastaouniversalsela"
+                ],
+                "fileName" => "filerg11"
+            ],
+            [
+                "rg" => "rg12",
+                "goal" => "goal12",
+                "relation" => [
+                    "relationXcolar"
+                ],
+                "fileName" => "filerg12"
+            ],
+            [
+                "rg" => "rg13",
+                "goal" => "goal13",
+                "relation" => [
+                    "relationXbastao",
+                    "relationbastaouniversalestopo",
+                    "relationestopoisoladorvelho"
+                ],
+                "fileName" => "filerg13"
+            ],
+            [
+                "rg" => "rg14",
+                "goal" => "goal14",
+                "relation" => [
+                    "relationXbastaouniversal",
+                    "relationbataouniversalcorda",
+                    "relationcordaestopo",
+                    "relationestopocorda"
+                ],
+                "fileName" => "filerg14"
+            ],
+            [
+                "rg" => "rg15",
+                "goal" => "goal15",
+                "relation" => [
+                    "relationchavecatracabastaouniversal",
+                    "relationXchavecatraca",
+                    "relationXbastaouniversal",
+                    "relationchavecatracaparafuso"
+                ],
+                "fileName" => "filerg15"
             ]
         ];
         foreach($relations as $relation)
