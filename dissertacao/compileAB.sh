9b2e42a5795d0e52934fd52bfbbe34546ace67cd
joneng2016 <jonathan_samara@hotmail.com>
2019-12-17 01:47:17 -0200

    entrega biblioteca

---
+++ dissertacao/compileAB.sh
@@ -0,0 +1,5 @@
+pdflatex abntex2-modelo-trabalho-academico.tex
+bibtex abntex2-modelo-trabalho-academico.aux
+pdflatex abntex2-modelo-trabalho-academico.tex
+pdflatex abntex2-modelo-trabalho-academico.tex
+
